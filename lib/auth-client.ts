d1ba088592dd8359c629edd54017c84ff94a9900
EnamulBokshi <enamulbokshi@gmail.com>
2026-02-17 17:59:37 +0600

    fix: update baseURL in authClient to use NEXT_PUBLIC_APP_URL

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,12 +1,12 @@
 import { env } from "@/env";
 import { createAuthClient } from "better-auth/react";
 
-const BACKEND_URL = env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+// const BACKEND_URL = env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 
 export const authClient = createAuthClient({
 
  fetchOptions: {
     credentials: "include",
   },
-  baseURL: '/api/auth',
+baseURL: process.env.NEXT_PUBLIC_APP_URL + "/api/auth"
 });
