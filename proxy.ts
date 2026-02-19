41e839ead5cd03fc9515ca61951d28ae7f266fd7
EnamulBokshi <enamulbokshi@gmail.com>
2026-02-19 23:12:44 +0600

    fix: refactor middleware to handle session token retrieval and improve fallback mechanism

--- proxy.ts
+++ proxy.ts
@@ -1,25 +1,52 @@
+// import { NextRequest, NextResponse } from "next/server";
+
+// export async function proxy(request: NextRequest) {
+//   const pathname = request.nextUrl.pathname;
+
+//   // Skip middleware for verify-email route
+
+//   if (pathname.startsWith("/verify-email")) {
+//     return NextResponse.next();
+//   }
+
+//   // Check for session token in cookies
+
+//   const sessionToken = request.cookies.get("better-auth.session_token");
+
+//   //* User is not authenticated at all
+
+//   if (!sessionToken) {
+//     return NextResponse.redirect(new URL("/login", request.url));
+//   }
+
+//   // Allow access if session exists
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/dashboard/:path*", "/admin-dashboard/:path*"],
+// };
+
+
+
 import { NextRequest, NextResponse } from "next/server";
 
-export async function proxy(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
-
-  // Skip middleware for verify-email route
 
   if (pathname.startsWith("/verify-email")) {
     return NextResponse.next();
   }
 
-  // Check for session token in cookies
+  const sessionToken =
+    request.cookies.get("__Secure-better-auth.session_token") ??
+    request.cookies.get("better-auth.session_token"); // fallback (local/dev)
 
-  const sessionToken = request.cookies.get("__Secure-better-auth.session_data");
-
-  //* User is not authenticated at all
-
+    console.log("Session token in middleware:", sessionToken);
   if (!sessionToken) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
-
-  // Allow access if session exists
 
   return NextResponse.next();
 }
